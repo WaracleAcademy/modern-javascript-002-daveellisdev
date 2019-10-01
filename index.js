4f7a6ece211c54a17eb1676bfa528b5f396d3072
Dave Ellis <dave@daveellis.dev>
2019-10-01 18:08:51 +0100

    Moved JS to external file

---
+++ index.js
@@ -0,0 +1,6 @@
+const things = ['Silverstone', 'Hockenheim', 'Albert Park', 'Interlagos'];
+const mainView = document.querySelector('#primaryView');
+                              
+mainView.innerHTML = `
+    <h2>Text</h2>
+`;

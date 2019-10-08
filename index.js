62ea39ad779c8a824c53578d0a09d918eeb043ea
Dave Ellis <dave@daveellis.dev>
2019-10-08 17:52:20 +0100

    Updated

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import { render } from './renderer';
+import * as data from './data.json';
+
+console.log(data.results);
 
 const things = ['Silverstone', 'Hockenheim', 'Albert Park', 'Interlagos'];
                               
 render(`
-<h2>Text</h2>
+<h2>YAY</h2>
 `);
--- renderer.js
+++ renderer.js
@@ -1,12 +1,4 @@
-function render(htmlContent) {
+export function render(htmlContent) {
     const mainView = document.querySelector('#primaryView');
-                              
     mainView.innerHTML = htmlContent;
 }
-
-render(`
-<h2>Text</h2>
-`)
-render(`
-<h1>Naaah</h1>
-`)
